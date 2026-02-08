f95f0f4482c73384f829c555bfec4f6ca45be16f
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2026-02-08 23:53:28 +0000

    Added boilerplate for ExpenseTracker class

---
+++ src/ExpenseTracker.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList; // Import the ArrayList class
+public class ExpenseTracker {
+    ArrayList<Expense> expenses = new ArrayList<>(); // Create an ArrayList object
+
+    public void addExpenses(Expense newExp){
+        expenses.add(newExp);
+    }
+
+    public void view(){
+
+    }
+
+    public void filter(){
+
+    }
+
+    public void save(){
+
+    }
+
+    public void load(){
+
+    }
+}
